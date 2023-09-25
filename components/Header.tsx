bbb4a615c8ac7c1366a64e840d910d4df6d5a9c1
Sebastian <sebastiansloma@gmail.com>
2023-09-25 10:11:24 +0200

    add image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <div>
+            <Image src='/public/logo.jpg' alt='next' width={50} height={50}/>
+        </div>
+    </div>
   )
 }
 
