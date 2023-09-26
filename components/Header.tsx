2a6128621538e8445880bbff0d407e191e6be7aa
Sebastian <sebastiansloma@gmail.com>
2023-09-26 12:45:59 +0200

    added logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,15 @@ import Image from 'next/image'
 function Header() {
   return (
     <div>
-        <div>
-            <Image src='/logo.jpg' alt='next' width={200} height={200}/>
-            <h2 className='text-[25px] text-red-500'>Discover</h2>
+        <div className='flex gap-3 items-center'>
+            <Image src='/logo.png' alt='next' width={50} height={50}/>
+            <h2 className='text-[25px] font-semibold text-red-500 tracking-widest'>Discover</h2>
         </div>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
     </div>
   )
 }
