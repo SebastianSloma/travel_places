9db8ea60b0f0a6e5c74c575a70191a8ab3ae82bb
Sebastian <sebastiansloma@gmail.com>
2023-09-29 13:53:56 +0200

    add class

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -3,8 +3,16 @@ import Image from 'next/image';
 
 function Hero() {
 	return (
-		<div>
-			<Image src='/bg.png' alt='bg' width={800} height={200} className='w-full' />
+		<div className='text-center'>
+			<div>
+
+			<Image src='/bg.png' alt='bg' width={800} height={200} className='w-full absolute mt-[-50px]' />
+			</div>
+			<div>
+				<h2 className='text-[55px] text-red-600 tracking-widest font-semibold'>
+					TRAVEL
+				</h2>
+			</div>
 		</div>
 	);
 }
