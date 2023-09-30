1082115ec10f4a30722f485bc33670e18e7c2e84
Sebastian <sebastiansloma@gmail.com>
2023-09-30 13:25:24 +0200

    added input

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -5,13 +5,23 @@ function Hero() {
 	return (
 		<div className='text-center'>
 			<div>
+				<Image
+					src='/bg.png'
+					alt='bg'
+					width={800}
+					height={200}
+					className='w-full absolute mt-[-50px]'
+				/>
 
-			<Image src='/bg.png' alt='bg' width={800} height={200} className='w-full absolute mt-[-50px]' />
-			</div>
-			<div>
-				<h2 className='text-[55px] text-red-600 tracking-widest font-semibold'>
-					TRAVEL
-				</h2>
+				<div>
+					<h2 className='text-[55px] text-red-600 tracking-widest font-semibold'>
+						TRAVEL
+					</h2>
+					<h2 className='text-[20px]'>Your Amazing City</h2>
+					<div className='mt-5 z-20 '>
+						<input type="text" placeholder='Search Anything' className='z-20 bg-white p-3 border-[1px] rounded-full px-5 w-[36%] shadow-sm' />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
