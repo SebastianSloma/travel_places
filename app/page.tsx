a06a78cff36fcc9cf2060712a0b0cb871bfaab8a
Sebastian <sebastiansloma@gmail.com>
2023-10-04 12:56:27 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import Hero from './Hero'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
   <div>
     <Hero/>
+
+    <PlaceList/>
   </div>
   )
 }
