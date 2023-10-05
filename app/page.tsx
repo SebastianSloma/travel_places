c59937b4da5c40a61c5dba0b465f0b13550ff0d2
Sebastian <sebastiansloma@gmail.com>
2023-10-05 10:50:51 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
+"use client"
+import Hero from '@/components/Hero'
+import PlaceList from '@/components/PlaceList'
 import Image from 'next/image'
-import Hero from '../components/Hero'
-import PlaceList from '../components/PlaceList'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  return (
-  <div>
-    <Hero/>
 
-    <PlaceList/>
-  </div>
+  const [placeList,setPlaceList]=useState([])
+  useEffect(()=>{
+     getPlaceList('Hotels in New York');
+
+  },[])
+
+  const getPlaceList=async(value:string)=>{
+    setPlaceList([]);
+    const result=await fetch("/api/google-place-api?q="+value)
+    const data=await result.json();
+
+    setPlaceList(data.resp.results);
+  }
+  return (
+    <div>
+        <Hero userInput={(value:string)=>getPlaceList(value)} />
+
+       {placeList? <PlaceList placeList={placeList} />:null}
+    </div>
   )
 }
